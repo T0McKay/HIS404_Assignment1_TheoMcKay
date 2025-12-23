c0eda41a6afaa4456c085546d822da466dacfc0c
T0McKay <mckay0534@hotmail.com>
2025-12-23 10:55:45 +0000

    Adding database load to system startup method

--- scripts/SystemClass.py
+++ scripts/SystemClass.py
@@ -1,7 +1,9 @@
 from UIManagerClass import UIManager
+from DatabaseManagerClass import DatabaseManager
 
 class System :
 
     @classmethod
     def systemStartup(cls) :
+        DatabaseManager.initialLoad()
         UIManager.displayLoginPage()
